f0222e476b3b10a50499a2bd3bc84771d4efe882
Oscar De Leeuw <odl@mediaan.com>
2017-12-01 10:50:32 +0100

    Day1

---
+++ Day1/Captcha.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day1
+{
+    class Captcha
+    {
+        public int[] Input { get; set; }
+
+        public Captcha(string input)
+        {
+            Input = ParseInput(input);
+        }
+
+        public int SolveCaptcha()
+        {
+            int last = Input.Last();
+            int sum = 0;
+
+            foreach (var i in Input)
+            {
+                if (last == i)
+                {
+                    sum += i;
+                }
+
+                last = i;
+            }
+
+            return sum;
+        }
+
+        public int SolveCaptcha2()
+        {
+            int sum = 0;
+
+            for (var i = 0; i < Input.Length / 2; i++)
+            {
+                int current = Input[i];
+                int opposite = Input[(Input.Length)/2 + i];
+
+                sum += current == opposite ? current*2 : 0;
+            }
+
+            return sum;
+        }
+
+        private int[] ParseInput(String input)
+        {
+            return input.Select(Convert.ToInt32).Select(i => i-48).ToArray();
+        }
+    }
+}
